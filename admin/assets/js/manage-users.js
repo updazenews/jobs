701028af69ce14a92e406bc794d427f54deec01d
updazenews <updazenews@gmail.com>
2026-01-25 10:04:04 +0000

    reading users table

--- admin/assets/js/manage-users.js
+++ admin/assets/js/manage-users.js
@@ -1,15 +1,77 @@
-import { signInWithEmailAndPassword } 
-from "/admin/node_modules/firebase-admin";
-import { auth } from "/assets/js/jobs.js";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const tbUsers = document.getElementById('tbUsers');
+import { db } from "../../../assets/js/jobs.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
+  const usersContainer = document.getElementById("tbUsers");
 
-    signInWithEmailAndPassword.getAuth().getUsers(auth).then((getUsersResult) => {
-        console.log('Successfully fetched user data:');
-        getUsersResult.users.forEach((userRecord) => {
-          console.log(userRecord);
-        });
+  try {
+    
+    const usersFer = collection(db, "users");
+    const q = query(
+      usersFer, 
+      orderBy("Full Name", "asc")
+    );
+    const snapshot = await getDocs(q);
+     if (snapshot.empty) {
+      usersContainer.innerHTML = `
+      <thead>
+        <tr>
+                <th scope="col">#</th>
+                <th scope="col">Full Name</th>
+                <th scope="col">Email Address</th>
+                <th scope="col">Header</th>
+                <th scope="col">Header</th>
+            </tr>
+        </thead>
+        <tbody>
+          <div class="col-12 text-center text-muted">
+            No job postings available at the moment.
+          </div>
+        <tbody>
+        
+      `;
+      return;
+    }
+      const users = snapshot.docs.map(doc => {
+      const data = doc.data();
+      
+      return {
+          fullName: data["Full Name"],
+          email: data["Email Address"],
+          header: data["Header"],
+          uid: doc.id 
+        };
       });
+    
+      
+
+  } catch (error) {
+    
+  }
+
+   // 🔽 YOUR EXISTING RENDERER (UNCHANGED)
+  function renderUsers(users){
+    usersContainer.innerHTML = "<tbody>";
+    users.forEach(user => {
+      const row = `
+        <tr>
+          <td>${user.fullName}</td>
+          <td>${user.email}</td>
+          <td>${user.header}</td>
+          <td><button class="btn btn-danger" onclick="deleteUser('${user.uid}')">Delete</button></td>
+        </tr>
+      `;
+      usersContainer.innerHTML += row;
+    });
+
+    usersContainer.innerHTML += "</tbody>";
+    
+  }
 });
