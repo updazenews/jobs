0638bd95c5ecd2ec34db1b24da79fdb38f48abe2
updazenews <updazenews@gmail.com>
2026-01-25 14:29:25 +0000

    Added sign out Page

---
+++ assets/js/sign-out.js
@@ -0,0 +1,12 @@
+import { db, auth } from "assets/js/jobs.js";
+import { auth } from "./jobs.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+        window.location.href = "/login";
+    }).catch((error) => {
+        // An error happened.
+    });
+});
