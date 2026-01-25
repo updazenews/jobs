4fffcd3cb7182c6daa72dc404a1e00d27d3d2c6b
updazenews <updazenews@gmail.com>
2026-01-25 15:16:58 +0000

    Corrected the Jobs Location

--- assets/js/sign-out.js
+++ assets/js/sign-out.js
@@ -1,5 +1,5 @@
 import { db} from "assets/js/jobs.js";
-import { auth } from "./jobs.js";
+import { auth } from "/jobs.js";
 import { signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 document.addEventListener('DOMContentLoaded', () => {
