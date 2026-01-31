dd406863d30982470944c1880571bef0c709b7b1
spmenze02 <spmenze@icloud.com>
2026-01-31 13:01:16 +0200

    Rectified the with the Correct Field

--- admin/assets/js/edit-job.js
+++ admin/assets/js/edit-job.js
@@ -16,14 +16,13 @@ document.addEventListener("DOMContentLoaded", async () =>{
     const params = new URLSearchParams(window.location.search);
     const inputCompanyName = document.getElementById("inputCompanyName");
     const inputJobTitle = document.getElementById("inputJobTitle");
-    const inputJobLocation = document.getElementById("inputJobLocation");
+    const inputJobLocation = document.getElementById("inputLocation");
     const inputJobType = document.getElementById("inputJobType");
-    const inputJobCategory = document.getElementById("inputJobCategory");
+    const inputWorkType = document.getElementById("inputWorkType");
     const inputJobDescription = document.getElementById("inputJobDescription");
-    const inputApplicationEmail = document.getElementById("inputApplicationEmail");
+    const inputClosingDate = document.getElementById("inputClosingDate");
     const inputApplicationURL = document.getElementById("inputApplicationURL");
-    const inputJobTags = document.getElementById("inputJobTags");
-    const jobForm = document.getElementById("jobForm");
+    const inputJobRequirements = document.getElementById("inputJobRequirements");
     const jobId = params.get("id");
 
     // Fetch existing job data
@@ -36,9 +35,11 @@ document.addEventListener("DOMContentLoaded", async () =>{
         inputJobTitle.value = jobData.title || "";
         inputJobLocation.value = jobData.location || "";
         inputJobType.value = jobData.jobType || "";
-        inputJobCategory.value = jobData.jobCategory || "";
+        inputWorkType.value = jobData.workType || "";
         inputJobDescription.value = jobData.description || "";
+        inputClosingDate.value = jobData.closingDate || "";
         inputApplicationURL.value = jobData.url || "";
+        inputJobRequirements.value = jobData.requirements || "";
         //inputJobTags.value = (jobData.jobTags || []).join(", ");
     }
     
