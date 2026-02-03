3585f73113a08dc629774b160d1f73d28effd619
spmenze02 <spmenze@icloud.com>
2026-02-03 15:42:57 +0200

    Added AdSence Fund Recovery Line

--- assets/js/jobs.js
+++ assets/js/jobs.js
@@ -17,3 +17,5 @@ const app = initializeApp(firebaseConfig);
 // ✅ EXPORT db ONCE
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
+(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();
